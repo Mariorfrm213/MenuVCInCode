51f57b5e5b64950eec09eb0176a94aaab254b32c
Mariorfrm213 <marioruiz@mario.local>
2016-03-31 18:59:02 -0700

    Initial Commit

---
+++ MenuVCInCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MenuVCInCode
+//
+//  Created by Mario Ruiz on 3/31/16.
+//  Copyright © 2016 Kenetic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MenuVCInCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MenuVCInCode
+//
+//  Created by Mario Ruiz on 3/31/16.
+//  Copyright © 2016 Kenetic Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
